9e535d3961169f71b0960f976b64ec9804eea9ce
asndev <asndev99@gmail.com>
2024-08-26 14:12:50 +0500

    hooks implemented

---
+++ test/hooks.spec.ts
@@ -0,0 +1,35 @@
+import { expect } from "chai";
+import Calculator from "../src/calculator";
+import chai from "chai";
+chai.should();
+
+describe.only("hooks test", () => {
+  let calc: Calculator;
+
+  before(() => {
+    console.log("before");
+    calc = new Calculator();
+  });
+
+  beforeEach(() => {
+    console.log("Before Each Executed");
+  });
+
+  it("Should return sum", () => {
+    const result = calc.add(2, 3);
+    expect(result).equal(5);
+  });
+
+  it("should return subtract", () => {
+    const result = calc.subtract(5, 3);
+    result.should.equal(2);
+  });
+
+  afterEach(() => {
+    console.log("After Each");
+  });
+
+  after(() => {
+    console.log("After");
+  });
+});
